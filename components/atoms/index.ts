3a6dd4f5cf3eb809ccc29ad43d625d04566fe02f
Zaki <zaki.maliki@ilcs.co.id>
2025-06-06 07:26:29 +0700

    fourth commit

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,5 +2,4 @@
 export { Button } from './Button';
 export { Input } from './Input';
 export { FormInput } from './FormInput';
-export { Modal } from './Modal';
 export { ErrorMessage } from './ErrorMessage'; 
